2a10c0644fc9835e6f9746645504725235e1e563
beepfish <beepfish@futurewidgets.eu>
2025-06-03 19:32:30 +0200

    searching and creating playlists

--- spotify_create_playlist/main.py
+++ spotify_create_playlist/main.py
@@ -1,3 +1,6 @@
+from typing import Optional
+import datetime
+
 from dotenv import load_dotenv
 import spotipy
 from spotipy import CacheFileHandler
@@ -7,11 +10,64 @@ from spotipy.oauth2 import SpotifyOAuth
 load_dotenv()
 
 
-def authenticate() -> spotipy.Spotipy:
-    scope = "user-library-read"
+class PlaylistCreator:
+    SCOPES = [
+        "playlist-read-private",
+        "playlist-read-collaborative",
+        "playlist-modify-private",
+        "playlist-modify-public",
+    ]
 
-    cache_handler = CacheFileHandler()
-    auth_manager = SpotifyOAuth(scope=scope, cache_handler=cache_handler)
-    return spotipy.Spotify(auth_manager=auth_manager)
+    def __init__(self):
+        self.sp = None
+
+    def authenticate(self):
+        cache_handler = CacheFileHandler()
+        auth_manager = SpotifyOAuth(scope=self.SCOPES, cache_handler=cache_handler)
+        self.sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    def search_tracks(self, query: str, limit: int = 1, offset: int = 0) -> Optional[dict]:
+        results = self.sp.search(query, limit, offset, type="track")
+        return results.get("tracks", {}).get("items", [])[0]
+
+    def find_user_playlist(self, name: str) -> Optional[dict]:
+        result = None
+        limit = 50
+        offset = 0
+        while True:
+            playlists = self.sp.current_user_playlists(limit, offset)
+
+            items = playlists.get("items", [])
+            for item in items:
+                item_name = item.get("name", "")
+                if item_name == name:
+                    result = item
+                    break
+
+            offset += limit
+
+            # break if there are no more results
+            if playlists.get("next") is None:
+                break
+
+            # break if a playlist was found
+            if result is not None:
+                break
+        
+        return result
+
+    def get_or_create_playlist(self, name: str = "Spotipy tracks"):
+        playlist = self.find_user_playlist(name)
+
+        if playlist is None:
+            user = self.sp.me()["id"]
+            playlist = self.sp.user_playlist_create(user, name, public = False)
+
+        return playlist
 
 
+if __name__ == "__main__":
+    creator = PlaylistCreator()
+    creator.authenticate()
+
+    print(creator.get_or_create_playlist())
