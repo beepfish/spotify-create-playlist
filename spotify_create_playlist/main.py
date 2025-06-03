f0a4bc2f124b97d2aa08b08f83f3d7a19b2e8043
beepfish <beepfish@futurewidgets.eu>
2025-06-03 20:57:58 +0200

    only add tracks that aren't already on the playlist

--- spotify_create_playlist/main.py
+++ spotify_create_playlist/main.py
@@ -28,7 +28,10 @@ class PlaylistCreator:
 
     def search_tracks(self, query: str, limit: int = 1, offset: int = 0) -> Optional[dict]:
         results = self.sp.search(query, limit, offset, type="track")
-        return results.get("tracks", {}).get("items", [])[0]
+        tracks = results.get("tracks", {}).get("items", [])
+
+        if len(tracks) > 0:
+            return tracks[0]
 
     def find_user_playlist(self, name: str) -> Optional[dict]:
         result = None
@@ -55,6 +58,27 @@ class PlaylistCreator:
                 break
         
         return result
+
+    def find_playlist_tracks(self, playlist_id: str) -> dict[str, bool]:
+        track_uris = {}
+        limit = 100
+        offset = 0
+        while True:
+            playlist_tracks = self.sp.playlist_tracks(playlist_id, limit=limit, offset=offset)
+
+            tracks = playlist_tracks.get("items", [])
+            for track in tracks:
+                uri = track.get("track", {}).get("uri")
+                if uri is not None:
+                    track_uris[uri] = True
+
+            offset += limit
+
+            # break if there are no more results
+            if playlist_tracks.get("next") is None:
+                break
+
+        return track_uris
 
     def get_or_create_playlist(self, name: str = "Spotipy tracks"):
         # search for existing playlist first
@@ -95,11 +119,40 @@ if __name__ == "__main__":
         print(f"Usage: {sys.argv[0]} <tracklist>")
         sys.exit(1)
 
-    # load file list
+    # load track list
     filename = sys.argv[1]
     tracks = dedupe_tracks(filename)
+    num_tracks = len(tracks)
 
     # init spotipy
     creator = PlaylistCreator()
     creator.authenticate()
     playlist = creator.get_or_create_playlist()
+    playlist_id = playlist["id"]
+    playlist_tracks = creator.find_playlist_tracks(playlist_id)
+
+    # search tracks and add to playlist
+    track_uris = []
+    for i, track in enumerate(tracks):
+        print(f"[{i + 1} of {num_tracks}] {track} -- ", end="")
+
+        result = creator.search_tracks(track)
+        if result is None:
+            print("TRACK NOT FOUND")
+            continue
+
+        uri = result.get("uri")
+        if uri is None:
+            print("TRACK HAS NO URI")
+            continue
+
+        exists = playlist_tracks.get(uri)
+        if exists:
+            print("ALREADY ON PLAYLIST")
+        else:
+            print("ADDED")
+            track_uris.append(uri)
+
+    if len(track_uris) > 0:
+        print(f"Adding {len(track_uris)} track(s) to playlist")
+        creator.sp.playlist_add_items(playlist_id, track_uris)
